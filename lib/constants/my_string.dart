0e09e5840330cf3ccb36bdfe183317ee2c7fe63b
Mohamed55ahmed <atf343069@gmail.com>
2022-05-27 04:14:54 +0200

    none

--- lib/constants/my_string.dart
+++ lib/constants/my_string.dart
@@ -8,7 +8,7 @@ String validationPhone = r'(^(?:[+0]9)?[0-9]{10,12}$)';
 const String usersCollectionKey = "users";
 const String statusCollectionKey = "status";
 const String gameRoomsCollectionKey = "gameRooms";
-const String callsCollectionKey = "calls";
+
 const String KUid = "uid";
 
 // const String Agora_Token =
